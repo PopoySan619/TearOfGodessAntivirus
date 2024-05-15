fa4eac7fd3fff4d4a7143685372b2875371fc464
PopoySan619 <54986918+PopoySan619@users.noreply.github.com>
2024-05-15 17:35:06 +0800

    Files Upload

---
+++ resources/views/frontend/master.blade.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- basic -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- mobile metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<!-- site metas -->
+<link rel="icon" href="images/logo.png" type="image/x-icon">
+<title>Tear Of Goddess</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content=""> 
+<!-- bootstrap css -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- style css -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<!-- Responsive-->
+<link rel="stylesheet" href="css/responsive.css">
+<!-- fevicon -->
+<link rel="icon" href="images/fevicon.png" type="image/gif" />
+<!-- Scrollbar Custom CSS -->
+<link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+<!-- Tweaks for older IEs-->
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<!-- owl stylesheets --> 
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+</head>
+<body>
+    <!-- header section start -->
+    <div class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="products.html">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="testimony.html">Testimony </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact us</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">              
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">GET STARTED</button>
+            </form>
+            <div class="search_icon">
+              <ul>
+                <li><a href="/register"><img src="images/user-icon.png"></a></li>
+              </ul>
+            </div>
+
+          </div>
+        </nav>
+      </div>
+    <!-- header section end -->
+    <!-- banner section start -->
+    <div class="banner_section layout_padding">
+      <div class="container">
+              <div class="row">
+                <div class="col-md-6">
+                  <h1 class="your_text">Tear Of Goddess</h1>
+                  <h1 class="Shows_text">Standard Antivirus</h1>
+                  <p class="there_text">Security score provides insight on your risk situation and security status along with helpful tips for enhancing device security.</p>
+                  <div class="start_bt"><a href="products.html">Start Now</a></div>
+                  <div class="read_bt"><a href="about.html">Read More</a></div>
+                </div>
+                <div class="col-md-6">
+                  <div class="images_1"><img src="images/tog.png"></div>
+                </div>
+              </div>
+            </div>
+              </div>
+            </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="banner_section_2">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-sm-12">
+              <div class="box_main">
+                <div class="internet_icon"></div>
+                <h4 class="broadband_text">Standard Antivirus</h4>
+                <p class="many_text">Get real-time, easy-to-understand visual snapshots of your digital well-being reports, along with recommendations to improve your digital safety and online privacy.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+              </div>
+              </div>
+            </div>
+          </div>
+        
+      </div>
+    </div>
+    <!-- banner section end -->
+    </div>
+    <!-- about section start -->
+    <div class="about_section layout_padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="image_2"><img src="images/img-2.png"></div>
+          </div>
+          <div class="col-md-6">
+            <h1 class="live_text">Award-winning Security and Privacy For Your Family's Digital World
+            </h1>
+            <p class="lorem_text">Tear Of Goddess award-winning antivirus for PC, Laptops, Mac, and Windows empowers people to live a secure digital life and browse the web confidently. More than security, peace of mind matters, and our advanced protection offers the toughest defense against viruses, ransomware, malware, Wi-Fi vulnerabilities, and more. Keeping millions of people safe & secure around the world, our all-in-one protection will make your family's digital life easy.</p>
+            <div class="online_bt_main">
+              <div class="online_bt"><a href="about.html">Award-winning</a></div>
+              <div class="online_bt1"><a href="#">Home</a></div>
+              <div class="read_bt1"><a href="about.html">Read More</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- about section end -->
+    <!-- product section start -->
+    <div class="product_section layout_padding">
+      <div class="container">
+        <h2 class="tariffs_text">Tear Of Goddess</h2>
+        <h1 class="choose_text">Antivirus</h1>
+        <p class="lorem_text1">All-in-one Smart Protection for your connected world
+          Secure your digital life against complex threats with our top antivirus product suite for your PC, laptop, and mobile devices.</p>
+        <div class="product_section2">
+          <div class="row">
+            <div class="col-lg-12 col-sm-2">
+              <h2 class="easy_text">Antivirus </h2>
+              <h1 class="internet_text">Standard</h1>
+              <div class="month_main clearfix">
+                <h1 class="dolor_text">28.99$</h1>
+                <h2 class="month_text">Best Deal</h2>
+              </div>
+              <div class="seemore_main">
+                <div class="see_more"><a href="products.html">SEE MORE</a></div>
+              </div>
+            </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- product section end -->
+    <!-- contact section start -->
+    <div class="contact_section layout_padding">
+      <div class="container">
+        <h1 class="check_text">Check ability to connect our services in your area</h1>
+        <div class="contact_section2">
+          <div class="addres_main">
+            <div class="input_bg">
+              <h3 class="fact_text">It is a long established fact that a reader will be distracted</h3>
+              <input type="text" class="address_text" placeholder="Enter your address" name="text">
+              <button type="button" class="get_bt">GET STARTED</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- contact section end -->
+    <!-- client section start -->
+    <div class="client_section layout_padding">
+      <div class="container">
+        <h1 class="client_taital">What our clients say</h1>
+        <div class="client_section2 layout_padding">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="client_image"><img src="images/img-3.png"></div>
+            </div>
+            <div class="col-sm-8">
+              <div class="miller_text">Tristan<span class="quote_icon"><img src="images/quote-icon.png"></span></div>
+              <p class="ipsum_text">
+                
+I had been using Tear Of Goddes Antivirus Pro for years and things were going well, this year I switched to the Total Security version and now my computer has become very fast, and thanks to the Parental Control features I can educate my children in using of the internet in a conscious way.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- client section end -->
+    <!-- subscribe section start -->
+    <div class="subscribe_section layout_padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <h1 class="subscribe_text">Subscribe Now</h1>
+          </div>
+          <div class="col-sm-8">
+            <div class="mail_main">
+              <input type="text" class="address_text1" placeholder="Enter your address" name="text">
+              <button type="button" class="get_bt1">GET STARTED</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  clsubscribe section end -->
+    <!-- copyright section start -->
+    <div class="copyright_section">
+      <div class="container">
+        <div class="border"></div>
+        <p class="copyright_text">2020 All Rights Reserved.</p>
+      </div>
+    </div>
+    <!-- copyright section end -->
+    <!-- Javascript files-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="js/plugin.js"></script>
+    <!-- sidebar -->
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/custom.js"></script>
+    <!-- javascript --> 
+    <script src="js/owl.carousel.js"></script>
+    <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>    
+</body>
+</html>
